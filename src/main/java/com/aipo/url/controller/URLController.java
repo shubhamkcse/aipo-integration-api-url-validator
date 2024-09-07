cabecd10c574bcf82772e2d93736217cb2e37f45
shubhamkcse <95348181+shubhamkcse@users.noreply.github.com>
2024-09-07 12:25:43 +0530

    Add existing project files to Git

--- src/main/java/com/aipo/url/controller/URLController.java
+++ src/main/java/com/aipo/url/controller/URLController.java
@@ -3,6 +3,7 @@ package com.aipo.url.controller;
 import com.aipo.url.service.URLService;
 import com.aipo.url.model.UrlResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,11 @@ public class URLController {
 
     @Autowired
     URLService urlService;
+
+    @GetMapping(value = "/health")
+    public ResponseEntity<String> getHealth() {
+        return new ResponseEntity<>("endpoint is healthy", HttpStatus.OK);
+    }
 
     @GetMapping("/aipo/url-checking")
     public ResponseEntity<UrlResponse> checkURl(@RequestParam String url) {
